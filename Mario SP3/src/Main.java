5e3052417a4ee0c8544b89d865685324c2563f81
cph-mm620 <70536109+cph-mm620@users.noreply.github.com>
2020-11-04 12:56:38 +0100

    Update Main.java

--- Mario SP3/src/Main.java
+++ Mario SP3/src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
     public static void main(String[] args){
+// MainController mc = new MainController();
+       // mc.runProgram();
 
 
+        //Customer stuff :P
+        Customer customer = new Customer(1,1,"");
+        customer.customerNumberGen();
+        customer.customerInput();
+
+        //TODO: Finde ud af en måde hvorpå den aktive ordre class kan gribe de/den nyoprettede Customer Class object.
 
 
     }
