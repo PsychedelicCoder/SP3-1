ded2e257c7f93d77783749bf4ce53338d38897ea
cph-mm620 <70536109+cph-mm620@users.noreply.github.com>
2020-11-05 15:32:02 +0100

    push to

--- Mario SP3/src/Main.java
+++ Mario SP3/src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
 
     public static void main(String[] args){
-// MainController mc = new MainController();
-       // mc.runProgram();
+PizzaBar pb = new PizzaBar();
+pb.runProgram();
 
 
-        //Customer stuff :P
-        Customer customer = new Customer(1,1,"");
-        customer.customerNumberGen();
-        customer.customerInput();
 
         //TODO: Finde ud af en måde hvorpå den aktive ordre class kan gribe de/den nyoprettede Customer Class object.
 
