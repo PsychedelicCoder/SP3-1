08e0fe30e7c1db3af9b5f349de2ee5b9b1143da3
OliverJenbo <71074579+OliverJenbo@users.noreply.github.com>
2020-11-05 13:14:26 +0100

    Update Menu.java

--- Mario SP3/src/Menu.java
+++ Mario SP3/src/Menu.java
@@ -1,2 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Menu {
+    /*
+@AUTHOR_JENBO
+ */
+
+// Der skal være et text document, hvor man skal kunne tilføje flere Pizza'er,
+// der er allerede givet de første 14 Pizza'er, vi skal så tilføje resten til vi rammer 30.
+// Denne klasse skal kunne læse dette text document.
+
+    public static void readFile() {
+        try {
+            File myMenu = new File("C:\\Users\\Oliver MARTIN\\IdeaProjects\\SP3\\Ressources\\Menu.csv");
+            Scanner myReader = new Scanner(myMenu);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
+
