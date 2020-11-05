93756a09405872ea75f138ac84c51ad7935671b6
cph-mm620 <70536109+cph-mm620@users.noreply.github.com>
2020-11-05 14:09:30 +0100

    Update PizzaBar.java

--- Mario SP3/src/PizzaBar.java
+++ Mario SP3/src/PizzaBar.java
@@ -1,2 +1,59 @@
+import java.util.Scanner;
+
 public class PizzaBar {
+    //Annika
+    Scanner sc = new Scanner(System.in);
+    int choice = 0;
+    int exitValue = 7;
+
+    public void runProgram(){
+
+        while (choice !=exitValue){
+
+            choice = sc.nextInt();
+            switch (choice){
+                case 1: seeMenu(); break;
+                case 2: seeCostumersOrder(); break;
+                case 3: newCostumerAndOrder(); break;
+                case 4: seeActiveOrder();break;
+                case 5: seeInactiveOrder();break;
+                case 6: moveOrder();
+                case 7: exit();break;
+                default: choice = exitValue;
+            }
+
+        }
+    }
+    private void seeMenu(){
+
+    }
+
+    private void seeCostumersOrder(){
+
+    }
+
+    private void newCostumerAndOrder(){
+        Customer customer = new Customer(1,1,"");
+        customer.customerNumberGen();
+        customer.customerInput();
+
+
+
+
+    }
+    private void seeActiveOrder(){
+
+    }
+
+    private void seeInactiveOrder(){
+
+    }
+
+    private void moveOrder(){
+
+    }
+
+    private void exit(){
+
+    }
 }
