faeee13b10ffef48d253c35c5c5aee3ba6437e4a
Annika-Rosenvinge <70583578+Annika-Rosenvinge@users.noreply.github.com>
2020-11-05 16:21:13 +0100

    Update ActiveOrder.java

--- Mario SP3/src/ActiveOrder.java
+++ Mario SP3/src/ActiveOrder.java
@@ -11,6 +11,8 @@ public class ActiveOrder {
     //price skal laves til en metode
     int price;
     Timestamp timeForOrder;
+    //pickup time skal laves til metode
+    Timestamp pickupTime;
 
     public ActiveOrder (String pizza, int price){
         this.pizza = pizza;
@@ -22,10 +24,17 @@ public class ActiveOrder {
 
     //tidsmetode
     //man udregne hvad tid og dato, ordren er lagt
-    public Timestamp calculateTimeForOrder(){
+    private Timestamp calculateTimeForOrder(){
         Date date = new Date();
         timeForOrder = new Timestamp(date.getTime());
         return timeForOrder;
+    }
+    //pick up time metode
+    //skal udregnes for pizzaernes præp tid.
+    private Timestamp pickupTime(){
+        
+        pickupTime = new Timestamp(date.getTime());
+        return pickupTime;
     }
     
     //Pris metode
